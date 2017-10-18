4e6319e250c8f7f3c5ef6ccafde8eef6cf3bcbd2
Vitor Rios <vrios81@gmail.com>
2017-10-18 10:56:50 -0300

    atualizado aula 1

--- aula 1.R
+++ aula 1.R
@@ -6,3 +6,12 @@ install_github('slidifyLibraries', 'ramnathv')
 "git@github.com:vrios/teste-Intro-Linguagem-R.git"
 "https://github.com/vrios/teste-Intro-Linguagem-R.git"
 library(Rcmdr)
+example(data.frame)
+length(letters)
+vetor1=LETTERS
+vetor2=1:26
+
+dataframe1=cbind(vetor1,vetor2) #organizando cada vetor como coluna
+dataframe2=rbind(vetor1,vetor2) #organizando cada vetor como linha
+class(dataframe1)
+dados=data.frame(x=vetor1,y=vetor2)
