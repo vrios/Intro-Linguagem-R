c6a1a2b8639608fa1d211cc6988b63d2bdc317c9
Vitor Rios <vrios81@gmail.com>
2017-10-19 11:33:15 -0300

    script aula 1

--- aula 1.R
+++ aula 1.R
@@ -1,17 +1,96 @@
-a = 5 + 5
-"words are strings"
-require('devtools')
-install_github('slidify', 'ramnathv')
-install_github('slidifyLibraries', 'ramnathv')
-"git@github.com:vrios/teste-Intro-Linguagem-R.git"
-"https://github.com/vrios/teste-Intro-Linguagem-R.git"
-library(Rcmdr)
-example(data.frame)
-length(letters)
-vetor1=LETTERS
-vetor2=1:26
+######################################################
+# Introdução à Linguagem R
+# Vitor Passos Rios
+# Universidade Federal da Bahia
+# https://github.com/vrios/Intro-Linguagem-R/wiki
+#####################################################
 
-dataframe1=cbind(vetor1,vetor2) #organizando cada vetor como coluna
-dataframe2=rbind(vetor1,vetor2) #organizando cada vetor como linha
-class(dataframe1)
-dados=data.frame(x=vetor1,y=vetor2)
+#####Usando o R como calculadora
+
+#Digite no R os códigos abaixo.
+#Dica: escreva neste arquivo, ao lado do comando, o que ele faz,
+#como no exemplo abaixo
+
+1 + 1 #soma os valores
+
+# O símbolo # representa um comentário, isto é, tudo que vier à esquerda
+# dele será ignorado quando o R ler o arquivo
+
+34 - 72.3
+
+# use pontos como separador de decimal
+78 / 3
+2 ^ 3
+3 * 2
+sqrt(2)
+pi
+log(x = 3,base = 10)
+exp(x = 1)
+#o que querem dizer estes valores dentro do parentêse? porque usamos o = aqui?
+
+#####Atribuição de valores e criação de objeto
+#O que os comandos abaixo fazem?
+objeto1=3
+objeto1
+objeto = 42
+objeto
+objeto.2 <- 42
+objeto.2
+# Os símbolos = e <- fazem basicamente a mesma coisa. Que coisa?
+objeto.cubico <- objeto^3
+resultado.1 <- (objeto.cubico / 3) + 7
+objeto.texto <- "texto sempre vem entre aspas"
+objeto.texto
+objeto.texto.2 <- "42"
+
+objeto.vetor.1 = 1:34
+objeto.vetor.1
+objeto.vetor2 = c(1, 74.5, 48)
+objeto.vetor2
+
+?seq
+objeto.vetor3 = seq(from = 20, to = 32, by = 0.5)
+
+# R também faz comparações entre objetos
+42 > 7
+objeto == objeto.2
+objeto == objeto.texto.2
+# o que aconteceu acima?
+7.1 <= 7.001
+?"<"
+
+#pedindo ajuda
+?pi
+?log
+??lm
+help(log)
+help.search("anova")
+
+#####Arquivos e diretórios
+# Como você descobre o diretório de trabalho?
+
+# Como você lista o conteúdo do diretório de trabalho?
+
+# Como você define o diretório de trabalho?
+
+# Como você carrega um arquivo de script?
+
+# Como você salva os objetos que criou?
+
+# Como você carrega os objetos que criou?
+
+# como ver quis objetos estão na sua área de trabalho?
+
+# como remover objetos da área de trabalho?
+
+##### Lidando com erros
+# O que acontece quando você digita os comandos abaixo? Como consertar cada erro?
+objetol
+objeto .texto
+Objeto
+source("chuchu.R")
+source(chuchu.R)
+setwd("C:/CavernaDoDragão")
+getwd
+Getwd()
+#dica: quando o R der erro, copie e cole a mensagem de erro no google
