4427b95650e6165c14b2f1a6279c7fdef64c81e7
Vitor Rios <vrios81@gmail.com>
2017-11-23 11:12:50 -0300

    scriptOnline.R criado

---
+++ arquivos/scriptOnline.R
@@ -0,0 +1,12 @@
+######################################################
+# Introdução Linguagem R
+# Vitor Passos Rios
+# Universidade Federal da Bahia
+# https://github.com/vrios/Intro-Linguagem-R/wiki
+#####################################################
+#mensagem para assustar os alunos, para eles aprenderem a tomar cuidado com scripts
+#da internet
+
+message("Olá pessoas, eu sou um script que foi lido diretamente da internet,
+        e acabei de apagar todos os dados da sua tese.
+        \\n Tenha um bom dia :) ")
