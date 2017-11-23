48e308e9f3086d53521bcf31421437583d03b71a
Vitor Rios <vrios81@gmail.com>
2017-11-23 12:52:52 -0300

    aula 3 e arquivos de exemplos

--- arquivos/scriptOnline.R
+++ arquivos/scriptOnline.R
@@ -8,6 +8,4 @@
 #mensagem para assustar os alunos, para eles aprenderem a tomar cuidado com scripts
 #da internet
 
-message("Olá pessoas, eu sou um script que foi lido diretamente da internet,
-        e acabei de apagar todos os dados da sua tese.
-        \\n Tenha um bom dia :) ")
+message("Olá pessoas, eu sou um script que foi lido diretamente da internet, e acabei de apagar todos os dados da sua tese. Tenha um bom dia :) ")
---
+++ aula_3.R
@@ -0,0 +1,32 @@
+######################################################
+# Introdução Linguagem R
+# Vitor Passos Rios
+# Universidade Federal da Bahia
+# https://github.com/vrios/Intro-Linguagem-R/wiki
+#####################################################
+
+#diferencie a função source da função load()
+
+#baixe para seu diretório de trabalho o arquivo toroidal.distance.R, disponível
+#em https://raw.githubusercontent.com/vrios/Intro-Linguagem-R/master/arquivos/toroidal.distance.R, e o arquivo distancias.csv, disponivel em https://raw.githubusercontent.com/vrios/Intro-Linguagem-R/master/arquivos/distancias.csv
+
+#Abra toridal.distances.R no R
+# O que você precisa fazer para que as funções contidas nesse arquivo estejam disponíveis para uso no R?
+
+# Faça o procedimento que você descreveu acima
+
+# dentro do script toroidal.distance.R, altere o nome da função toroidal.distances para distance.matrix (substitua o nome dentro do arquivo) e salve o arquivo
+# Carregue o arquivo distancias.csv para dentro do objeto distancias
+#chame a função distance.matrix() com o objeto distancias, e o argumento tam = 10
+
+# o que aconteceu?
+
+# O que você precisa fazer para que você possa usar a função distance.matrix?
+
+# Usando a linha de comando, importe o arquivo dragoes.xsls para o objeto DnD
+
+# Confira a estrutura do arquivo
+
+# Há algum NA? houve algum problema com o cabeçalho?
+
+# Refaça o script da aula 2, salvando o objeto caixeta com a coluna coletor e desvio em um arquivo chamado caixeta_com_desvio.csv
