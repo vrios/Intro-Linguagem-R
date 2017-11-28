e00192540a5b73642252c853133132c1fac22133
Vitor Rios <vrios81@gmail.com>
2017-11-28 09:39:37 -0300

    scriptOnline mais curel }:P

--- arquivos/scriptOnline.R
+++ arquivos/scriptOnline.R
@@ -7,7 +7,8 @@
 #####################################################
 #mensagem para assustar os alunos, para eles aprenderem a tomar cuidado com scripts
 #da internet
+pessoa = Sys.getenv("USERNAME")
 workDir=getwd()
 
-message(paste("Olá pessoa, eu sou um script que foi lido diretamente da internet, e acabei de apagar tudo que estava em ",workDir,", além de todos os dados da sua tese / dissertação. Tenha um bom dia :) ", sep=""))
+message(paste("Olá ", pessoa, ", eu sou um script que foi lido diretamente da internet, e acabei de apagar tudo que estava em ",workDir,", além de todos os dados da sua tese / dissertação. Tenha um bom dia :) ", sep=""))
 
