9d7e00332d26a2ca6511d84c66c7ec48c354446b
Vitor Rios <vrios81@gmail.com>
2017-12-07 11:34:20 -0300

    Aula 6

---
+++ aulas/aula_6/aula_6.R
@@ -0,0 +1,14 @@
+######################################################
+# Introdução Linguagem R
+# Vitor Passos Rios
+# Universidade Federal da Bahia
+# https://github.com/vrios/Intro-Linguagem-R/wiki
+#####################################################
+
+
+#Faça uma função que converta graus Celsius em Fahrenheit ou vice-versa. A função deve ter um argumento output, com valores "C" ou "F". A função deve decidir a conversão com base nesse parâmetro, e rejeitar qualquer outro valor
+
+# Escreva uma função que receba um vetor ou dataframe numérico, e devolva um objeto de dimensões iguais, com cada valor substituído por "par" ou "impar", a depender de se o valor é par ou impar. Se o objeto não for numérico, a função deve parar antes de fazer qualquer coisa e retornar um erro. Dica: você pode definir uma função dentro de outra, e pense com a família apply
+
+# Crie uma função chamada GFM (sigla para Generalized FuBá Model), que recebe um número inteiro inicial (chamado inicial), um numero inteiro final (chamado final), um vetor de qualquer tamanho de palavras (chamado strings), um vetor de numeros reais do mesmo tamanho que strings (chamado modulos). A função deve retornar vetor com uma sequência de inteiros indo de inicial até final, porém se o número for múltiplo de um dos valores de modulo, ele deve ser substuído pelo elemento correspondente do vetor strings. Se o número for múltiplo de mais de um elemento de modulos, deverá ser substuído pelo conjunto de termos equivalentes de strings, sem separação entre eles. A função deverá se recusar a executar, com uma mensagem de erro informando o tipo do erro, se qualquer um dos argumentos for do tipo errado, se modulo e string tiverem tamanhos diferentes, ou se o dia for sexta feira 13. A data não pode ser um argumento da função. Use o menor número de if() e else() que conseguir
+
