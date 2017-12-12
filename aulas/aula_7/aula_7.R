70299d55a74cc38d5420aa902da6cabc78d641a1
Vitor Rios <vrios81@gmail.com>
2017-12-11 22:27:58 -0300

    aula_7.R adicionado

---
+++ aulas/aula_7/aula_7.R
@@ -0,0 +1,38 @@
+######################################################
+# Introdução Linguagem R
+# Vitor Passos Rios
+# Universidade Federal da Bahia
+# https://github.com/vrios/Intro-Linguagem-R/wiki
+#####################################################
+
+#####Usando Git com RStudio#####
+
+#Primeiro, crie sua conta educacional no GitHub e configure o Git com seu nome e email (você já deveria ter feito isso nas Atividades preliminares)
+
+#Clone o repositório https://github.com/vrios/exemplo-curso-r para sua máquina
+
+#Na sua máquina, crie um arquivo chamado chuchu.R, edite, salve, e dê um commit.
+
+#Dê um pull, seguido de um push. O que aconteceu e porque?
+
+#O que é um pull request?
+
+#O que é um fork?
+
+#####Criando seu primeiro repositório no GitHub#####
+# Crie um repositório (público ou privado, como queira) chamado BIOC71-CURSO-R-UFBA
+
+# Coloque uma descrição, um arquivo de licença e um .gitignore
+
+# Clone ele para sua máquina, criando um projeto do RStudio
+
+# No diretório do projeto do RStudio, crie Uma pasta para cada aula do curso, com o nome seguindo o padrão aula_1, aula_2, etc
+
+# Dentro de cada pasta, coloque os scripts correspondentes
+
+# Para cada pasta, dê um commit separado, somente com os arquivos correspondentes àquela aula
+
+# Após dar todos os commits, dê um push pro remote
+
+# No GitHub, na página inicial do repositório, crie um novo arquivo chamado aula_0.R . Nele, salve o script das atividades preliminares do curso (voC~e fez as atvidades preliminares, certo? *certo?*?). Salve o arquivo no GitHub com uma commit message descritiva
+
